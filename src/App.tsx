82e8ef6f2ad70a7bc3bb2e58d2f65718f7ad9f35
Suren Nikoghosyan <snikoghosyan@vmware.com>
2023-02-04 17:21:42 +0400

    -Added TopBar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import './App.css';
+   
 
 import SideBarLeft from "./components/SideBarLeft";
+import TopBar from "./components/TopBar";
 
 function App() {
   return (
     <div className="App">
+      <TopBar/>
      <SideBarLeft/>
     </div>
   );
